4e65e333df17157faea9562f4de3b574617c87ac
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-19 22:13:02 +0300

    CollectionViewCell class added

--- Superheroes/Views/CollectionViewCell.swift
+++ Superheroes/Views/CollectionViewCell.swift
@@ -8,5 +8,16 @@
 import UIKit
 
 final class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = 15
+        }
+    }
     
+    func configure(with superhero: Superhero) {
+        mainLabel.text = superhero.name
+        let imageURL = URL(string: superhero.images.lg)
+        
+    }
 }
