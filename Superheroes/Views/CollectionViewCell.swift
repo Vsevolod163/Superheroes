da55147be573f4e8c4dd5d8c58172111df244692
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-19 22:07:54 +0300

    Class CollectionViewCell added

---
+++ Superheroes/Views/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Superheroes
+//
+//  Created by Vsevolod Lashin on 19.05.2023.
+//
+
+import UIKit
+
+final class CollectionViewCell: UICollectionViewCell {
+    
+}
